976d057d5a0950bd97d525c6707046bf77488646
loint <loinguyentrung@gmail.com>
2014-12-09 00:03:32 +0700

    Fix /var/www does not exist

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ folder_name = 'server-development'
 database_folder = 'database-manager'
 
 modules = [
-    'tools', 
-    'node', 
+    'tools',
+    'node',
     'memcached', #ok v
     'php', #ok v
     'phalcon', #ok v
     'zephir', #ok v
-    'mongo', #ok v   
+    'mongo', #ok v
     'redis', #ok v
     'varnish', #ok v
     'haproxy', #ok v
@@ -35,7 +35,7 @@ ftp = {
 
 from os import *
 
-tmp = path.abspath(curdir)
+tmp = '/tmp'
 
 paths = {
     'package': path.realpath(tmp)+ '/' + folder_name,
@@ -44,80 +44,91 @@ paths = {
     'host': '/home',
 }
 
-def res(file_name): 
+def run(cmd):
+    system(cmd)
+
+def res(file_name):
     return paths['config'] + '/' + file_name
+
+def copy(file, destination):
+    return 'cp -r ' + file + ' ' + destination
+
+def create(folder):
+    return 'mkdir -p ' + folder
+
+def delete(file):
+    return 'rm -rf ' + file
+
+def yum(names):
+    return 'yum install -y ' + names
 
 configs = {
     'php': [
-        'cp -r '+res('php.ini') + ' /etc/php.ini',
-        'rm -rf /etc/php.d/json.ini',
-        'cp -r '+res('yaml.ini') + ' /etc/php.d/yaml.ini', 
+        copy(res('php.ini'), '/etc/php.ini'),
+        delete('/etc/php.d/json.ini'),
+        copy(res('yaml.ini'), '/etc/php.d/yaml.ini')
     ],
     'nginx': [
-        'cp -r '+res('nginx.conf') + ' /etc/nginx/nginx.conf',
-        'cp -r '+res('nginx_default.conf') + ' /etc/nginx/conf.d/default.conf'
+        copy(res('nginx.conf'), '/etc/nginx/nginx.conf'),
+        copy(res('nginx_default.conf'), '/etc/nginx/conf.d/default.conf')
     ],
     'phalcon': [
-        'cp -r '+res('phalcon.ini') + ' /etc/php.d/phalcon.ini'
+        copy(res('phalcon.ini'), '/etc/php.d/phalcon.ini')
     ],
     'proftpd': [
-        'cp -r '+res('proftpd.conf') + ' /etc/proftpd.conf', 
-        "sed -i 's,\r,,;s, *$,,' /etc/proftpd.conf", 
+        copy(res('proftpd.conf'), '/etc/proftpd.conf'),
+        "sed -i 's,\r,,;s, *$,,' /etc/proftpd.conf"
         'setenforce 0',
-        'useradd '+ftp['username'], 
+        'useradd '+ftp['username'],
         'echo "'+ftp['username']+':'+ftp['password']+'" | chpasswd',
         'chown '+ftp['username']+':'+ftp['username']+' '+paths['host']+'/'+app['app']+' -R ',
-    ], 
+    ],
     'mongo':[
-        'cp -r '+res('mongo.ini') + ' /etc/php.d/mongo.ini'
+        copy(res('mongo.ini'), '/etc/php.d/mongo.ini')
     ],
     'redis':[
-        'cp -r '+res('redis.ini') + ' /etc/php.d/redis.ini'
+        copy(res('redis.ini'), '/etc/php.d/redis.ini')
     ],
     'app': [
         'TMP=`pwd`',
-        'rm -rf '+paths['host']+'/'+app['app'],
-        'phalcon create-project '+app['app'],
-        'chown '+ftp['username']+':'+ftp['username']+' '+app['app']+' -R',  
-        'mv ./'+app['app']+' '+paths['host']+'/'+app['app'],
-        'chmod -R a+w '+paths['host']+'/'+app['app']+'/app/cache',
+        delete(paths['host'] + '/' + app['app']),
+        'phalcon create-project ' + app['app'],
+        'chown ' + ftp['username'] + ':' + ftp['username'] + ' ' + app['app'] + ' -R',
+        'mv ./' + app['app'] + ' ' + paths['host'] + '/' + app['app'],
+        'chmod -R a+w ' + paths['host'] + '/' + app['app'] + '/app/cache',
         'cd $TMP',
-        'cp -r '+res('zep') + ' /usr/bin/zep'
+        copy(res('zep'), '/usr/bin/zep')
     ],
 }
 
 
-def run(cmd):
-    system(cmd)
-
-
 def load():
-    run('cd '+tmp)
-    run('yum install -y unzip wget')
-    run('wget '+git+' -O ' + folder_name + '.zip')
-    run('rm -rf ./' + folder_name)
+    run('cd ' + tmp)
+    run(yum('unzip wget'))
+    run('wget ' + git + ' -O ' + folder_name + '.zip')
+    run(delete('./' + folder_name))
     run('unzip ./' + folder_name)
-    run('rm -rf ' + folder_name + '.zip')
-    run('cp -r '+res('mongo.repo')+' /etc/yum.repos.d/mongodb.repo')
+    run(delete(folder_name + '.zip'))
+    run(copy(res('mongo.repo'), '/etc/yum.repos.d/mongodb.repo'))
     run('chmod +x ./' + folder_name + '/modules/*')
-    run('rm -rf ./' + database_folder)
-    run('cp -r ./' + folder_name + '/' + database_folder + ' /var/www/' + database_folder)
-
+    run(delete('./' + database_folder))
+    run(create('/var/www'))
+    run(copy('./' + folder_name + '/' + database_folder, ' /var/www/' + database_folder))
 
 
 def main():
     load()
-    run('cd '+tmp)
+    run('cd ' + tmp)
 
     for module in modules:
-        module_path = paths['module'] + '/' + module+'.sh'
+        module_path = paths['module'] + '/' + module + '.sh'
         run('sh '+module_path)
         if module in configs.keys():
             for cmd in configs[module]:
                 run(cmd)
 
-    run('chmod +x '+paths['package'] + '/service.py')
-    run('mv '+paths['package'] + '/service.py /usr/bin/app')
-    run('rm -rf '+tmp+'/' + folder_name)
+    run('chmod +x ' + paths['package'] + '/service.py')
+    run('mv ' + paths['package'] + '/service.py /usr/bin/app')
+    run('rm -rf '+ tmp + '/' + folder_name)
 
 main()
