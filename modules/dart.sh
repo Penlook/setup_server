22672bf1c06708b68d26e0f29e2346423c01d5b9
naviet <naviet2308@gmail.com>
2015-02-01 19:09:12 +0700

    fix wrong uncompress command

--- modules/dart.sh
+++ modules/dart.sh
@@ -3,7 +3,7 @@ TMP=/usr/local/src
 sudo mkdir -p $TMP
 cd $TMP
 wget https://storage.googleapis.com/dart-archive/channels/stable/release/42828/sdk/dartsdk-linux-ia32-release.zip
-tar -xvf dartsdk-linux-ia32-release.zip
+unzip dartsdk-linux-ia32-release.zip
 rm -rf dartsdk-linux-ia32-release.zip
 export PATH=$PATH:$TMP/dart-sdk/bin
 export GOPATH=$HOME
