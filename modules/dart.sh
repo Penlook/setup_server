edd0d4db3a13b96d46b59c77888985aec0bd68d2
naviet <naviet2308@gmail.com>
2015-02-03 01:02:07 +0700

    test fix bug install dart-sdk

--- modules/dart.sh
+++ modules/dart.sh
@@ -5,7 +5,7 @@ cd $TMP
 wget https://storage.googleapis.com/dart-archive/channels/stable/release/42828/sdk/dartsdk-linux-ia32-release.zip
 unzip dartsdk-linux-ia32-release.zip
 rm -rf dartsdk-linux-ia32-release.zip
-
+sudo chmod 755 dart-sdk
 DARTSCRIPT="/etc/profile.d/dartlang.sh"
 sudo rm -rf $DARTSCRIPT
 sudo touch $DARTSCRIPT && sudo chmod a+w+x $DARTSCRIPT
