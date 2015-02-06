2f4c765bfd94303946714418e3f2495f01d81ace
loint <loint@penlook.com>
2015-02-06 23:47:41 +0700

    allow yum update

--- modules/tools.sh
+++ modules/tools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-yum update
 yum groupinstall -y 'Development Tools'
+yum update -y
 yum install -y curl nano git m4 bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel gcc libtool libtool-ltdl-devel
 yum install -y automake autoconf ncurses-devel libxslt groff pckgconfig libedit libedit-devel
 yum install -y pcre-devel clang pcre-devel
