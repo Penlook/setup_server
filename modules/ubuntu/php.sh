74bd14187320f5b7b330bceef15df1956ec8ec8b
loint <loint@penlook.com>
2015-03-01 07:26:16 +0700

    Update php to PHP 5.6

--- modules/ubuntu/php.sh
+++ modules/ubuntu/php.sh
@@ -1,1 +1,2 @@
+sudo add-apt-repository ppa:ondrej/php5-5.6
 sudo apt-get install php5 -y
--- modules/ubuntu/tools.sh
+++ modules/ubuntu/tools.sh
@@ -1,4 +1,5 @@
 # INSTALL ESSENTIAL
 sudo apt-get update
+sudo apt-get install python-software-properties
 sudo apt-get -y install tcl8.5 build-essential
 sudo apt-get -y install zip unzip mercurial
