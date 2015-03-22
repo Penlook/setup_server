c403e834e29c40545c3a8161e168c9f7ce700408
naviet <naviet2308@gmail.com>
2015-03-23 00:38:45 +0700

    implement setup production server

--- modules/ubuntu/production.sh
+++ modules/ubuntu/production.sh
@@ -1,1 +1,13 @@
-echo "SETTING UP PRODUCTION"
+# Production server
+
+function install {
+	sudo chmod +x ./modules/$1.sh
+	./modules/$1.sh
+}
+
+install tools
+install gcc
+install go
+install python
+install php
+install nginx
